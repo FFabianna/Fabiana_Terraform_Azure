913fe5aff5745b86e97633e96ef0c2350ce948ae
FFabianna <93838575+FFabianna@users.noreply.github.com>
2025-03-04 07:50:51 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "111111"
+
+}
+
+
+
+resource "azurerm_resource_group" "lab_plataformas_rg" {
+  name     = "lab_plataformas_rg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "fabiana_ask1" {
+  name                = "fabiana_ask1"
+  location            = azurerm_resource_group.lab_plataformas_rg.location
+  resource_group_name = azurerm_resource_group.lab_plataformas_rg.name
+  dns_prefix          = "fabiana1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.fabiana_ask1.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.fabiana_ask1.kube_config_raw
+
+  sensitive = true
+}
